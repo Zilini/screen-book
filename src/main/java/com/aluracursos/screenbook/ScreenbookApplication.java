5083e08ec9c04a035e4806224848f6c98ac47241
Michael Ziliani <zililanimichael7@gmail.com>
2025-06-16 16:13:14 -0500

    configurando application.properties

--- src/main/java/com/aluracursos/screenbook/ScreenbookApplication.java
+++ src/main/java/com/aluracursos/screenbook/ScreenbookApplication.java
@@ -1,12 +1,15 @@
 package com.aluracursos.screenbook;
 
 import com.aluracursos.screenbook.principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenbookApplication  implements CommandLineRunner {
+	@Autowired
+	private SerieRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenbookApplication.class, args);
@@ -14,7 +17,7 @@ public class ScreenbookApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(re);
 		principal.mostrarMenu();
 	}
 }
--- src/main/java/com/aluracursos/screenbook/repository/LibroRepository.java
+++ src/main/java/com/aluracursos/screenbook/repository/LibroRepository.java
@@ -3,5 +3,8 @@ package com.aluracursos.screenbook.repository;
 import com.aluracursos.screenbook.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+    Optional<Libro> findByTituloContainsIgnoreCase();
 }
